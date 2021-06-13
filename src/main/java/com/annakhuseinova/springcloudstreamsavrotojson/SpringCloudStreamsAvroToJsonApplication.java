7ce00a98ccc38132339cb18c2d0dacc91cea172f
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-13 12:54:54 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamsavrotojson/SpringCloudStreamsAvroToJsonApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsavrotojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsAvroToJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsAvroToJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springcloudstreamsavrotojson/SpringCloudStreamsAvroToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsavrotojson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsAvroToJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
